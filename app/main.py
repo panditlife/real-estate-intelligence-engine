9f8a1cfc43fdd5bbef42f87c50c5a81a466f5212
Pankaj Pandit <ppandit2829@gmail.com>
2026-03-03 17:10:59 +0530

    add health checkup

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from datetime import datetime
 
 app = FastAPI(title="Real Estate Intelligence Engine")
 
 @app.get("/")
 def root():
     return {"message": "Real Estate Intelligence Engine is running"}
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "ok",
+        "timestamp": datetime.utcnow()
+    }
